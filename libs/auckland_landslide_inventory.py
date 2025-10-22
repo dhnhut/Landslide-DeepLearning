7df407e6f4116965a5a97983f5565c950fb9d29b
dhnhut@gmail.com <dhnhut@gmail.com>
2025-10-22 18:13:14 +1300

    Fetch all data layers

--- libs/auckland_landslide_inventory.py
+++ libs/auckland_landslide_inventory.py
@@ -3,6 +3,13 @@ import os
 import urllib.request
 import geopandas as gpd
 
+def layers():
+    return {
+        "lines": 0,
+        "polygons": 1,
+        "points": 2
+    }
+
 # Source https://www.arcgis.com/home/item.html?id=f7ca84d9c1524f99ab94e03b547cd143#data
 # Currently, there are total 146,813 records
 def fetch_data(output_dir, num_records=999999999, batch_size=2000):
@@ -17,29 +24,24 @@ def fetch_data(output_dir, num_records=999999999, batch_size=2000):
     os.makedirs(output_dir, exist_ok=True)
 
     for start in range(0, num_records, batch_size):
+        geojson_data = {}
+
+        for layer_name, layer_id in layers().items():
+            url = f'https://services-ap1.arcgis.com/9R0qvCUXav3QPG1F/arcgis/rest/services/ValidatedLandslides/FeatureServer/{layer_id}/query?where=1%3D1&outFields=*&outSR=4326&f=geojson&resultRecordCount={num_records}&resultOffset={start}'
         
-        print(f"Fetching records {start} to {min(start + batch_size, num_records)}")
-
-        url = f'https://services-ap1.arcgis.com/9R0qvCUXav3QPG1F/arcgis/rest/services/ValidatedLandslides/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=geojson&resultRecordCount={num_records}&resultOffset={start}'
-
-        try:
-            with urllib.request.urlopen(url) as response:
-                if response.getcode() == 200:
-                    json_string = response.read().decode('utf-8')
-                    geojson_data = json.loads(json_string)
-                else:
-                    print(f"Error fetching data: {response.getcode()}")
-                    return None
-        except Exception as e:
-            print(f"An error occurred: {e}")
-            return None
-        
-        gdf = gpd.GeoDataFrame.from_features(geojson_data["features"])
-
-        if not gdf.empty:
-            gdf.to_file(f"{output_dir}/raw_data.gpkg", driver='GPKG', layer='landslides', mode='a')
-            gdf.to_file(f"{output_dir}/raw_data.geojson", driver='GeoJSON')
-        else:
-            print("Warning: No features returned; skipping shapefile write.")
-            break
-    
+            print(f"Fetching records {start} to {min(start + batch_size, num_records)} for layer {layer_name}...")
+            try:
+                with urllib.request.urlopen(url) as response:
+                    if response.getcode() == 200:
+                        json_string = response.read().decode('utf-8')
+                        geojson_data[layer_name] = json.loads(json_string)
+                    else:
+                        print(f"Error fetching data: {response.getcode()}")
+                        return None
+            except Exception as e:
+                print(f"An error occurred: {e}")
+                return None
+            gdf = gpd.GeoDataFrame.from_features(geojson_data[layer_name]["features"])
+            if not gdf.empty:
+                gdf.to_file(f"{output_dir}/inventory.gpkg", driver='GPKG', layer=layer_name, mode='a')
+                gdf.to_file(f"{output_dir}/inventory_{layer_name}.geojson", driver='GeoJSON')
